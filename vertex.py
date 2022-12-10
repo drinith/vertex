26789ae40cab2d3385f9b8b6da4f5e753f18481e
drinith <felpmel@gmail.com>
2022-12-10 20:47:28 -0300

    greedy

--- vertex.py
+++ vertex.py
@@ -43,7 +43,7 @@ class Vertex:
 
     #https://www.geeksforgeeks.org/vertex-cover-problem-set-1-introduction-approximate-algorithm-2/
     # The function to print vertex cover
-    def printVertexCover(self):
+    def print_vertex_cover_approximate(self):
          
         # Initialize all vertices as not visited.
         visited = [False] * (self.V)
@@ -79,6 +79,42 @@ class Vertex:
                  
         print()
 
+    def vertex_cover_greedy(self,input_graph):
+        # YOUR CODE HERE
+        cover = []
+        valid, num_edge = self.valid_cover(input_graph, cover)
+        
+        while not valid:
+            m = [x for x in range(0, len(num_edge)) if num_edge[x] == max(num_edge)][0]
+            cover.append(m)
+            valid, num_edge = self.valid_cover(input_graph, cover)
+            
+        return cover
+
+    def valid_cover(self,graph, cover):
+        valid = True
+        num_edge = [0] * len(graph)
+        for i in range(0, len(graph)):
+            for j in range(i, len(graph)):
+                if graph[i][j] == 1:
+                    if (i not in cover) and (j not in cover):
+                        valid = False
+                        num_edge[i] += 1
+                        num_edge[j] += 1
+        return valid, num_edge
+
+    #Change edges and nodes list in matrix adjacency
+    def change_to_matrix_adjacency(self,edges,nodes):
+
+        size = len(nodes)
+        #create matrix with size list nodes
+        matrix_adjacency = [[0]*size for i in range(size)]  
+        #fill matrix with position tuple of egde list
+        for edge in edges:
+            matrix_adjacency[edge[0]][edge[1]]+=1
+            matrix_adjacency[edge[1]][edge[0]]+=1
+        
+        return matrix_adjacency
 
 if __name__=='__main__':
 
@@ -92,6 +128,12 @@ if __name__=='__main__':
     edges = list(graph.edges)
 
     v = Vertex()
+
+
+    #transformo o que tenho de grafo em lista e transformo para uma matriz
+    matriz =  v.change_to_matrix_adjacency(edges,nodes)
+
+    print(v.vertex_cover_greedy(matriz))
 
     print(f'nodes: {nodes}\nedges: {edges}')
 
